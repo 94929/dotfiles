457f5eb527540ab0004a864f2ae8654248b9d4d9
Jinsung <jinsung.ha929@gmail.com>
2020-09-03 00:28:56 +0900

    Use PWD instead of HOME/dotfiles for current dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
-# set-up pre-requisites
-declare -r DOTFILES=$HOME/.dotfiles
-
 # install brew if not exists
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# 
+# install packages using brew
 brew update
 brew tap homebrew/bundle
-brew bundle --file=$HOME/.dotfiles/Brewfile
+brew bundle --file=$PWD/.brewrc
 
-#
-[ ! -f $HOME/.gitconfig ] && ln -nfs $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
-[ ! -f $HOME/.gitignore_global ] && ln -nfs $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
+# if .gitconfig does not exist under the home directory, link one from here
+[ ! -f $HOME/.gitconfig ] && ln -nfs $PWD/.gitrc $HOME/.gitconfig
 
-#
+# change default shell to zsh, install oh-my-zsh, link .zshrc if not exist
 chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-[ ! -f $HOME/.zshrc ] && ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
+[ ! -f $HOME/.zshrc ] && ln -nfs $PWD/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
-# 
-source $HOME/dotfiles/.osx
+# source macOS configuration file
+source $PWD/.osxrc
 
