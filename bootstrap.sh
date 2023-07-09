dc2f3d06fe87521b5e676deb0aca80fee2578734
Jinsung Ha <jsung5381@naver.com>
2023-07-09 23:18:04 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ npm install --global pure-prompt
 
 # if .mackup.cfg does not exist under the home directory, link one from here
 [ ! -f $HOME/.mackup.cfg ] && ln -nfs $PWD/.mackuprc $HOME/.mackup.cfg
-#mackup restore
+mackup restore
 
 # if .gitconfig does not exist under the home directory, link one from here
 [ ! -f $HOME/.gitconfig ] && ln -nfs $PWD/.gitrc $HOME/.gitconfig
@@ -21,6 +21,8 @@ npm install --global pure-prompt
 # change default shell to zsh, install oh-my-zsh, link .zshrc
 chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 ln -nfs $PWD/.aliases $HOME/.aliases && ln -nfs $PWD/.exports $HOME/.exports && ln -nfs $PWD/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
