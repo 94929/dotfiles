138609c9e0162584065d8f66e12d31ea0003d733
Jinsung Ha <jsung5381@naver.com>
2023-07-09 23:35:37 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,7 @@ mackup restore
 chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ln -nfs $PWD/.aliases $HOME/.aliases && ln -nfs $PWD/.exports $HOME/.exports && ln -nfs $PWD/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
